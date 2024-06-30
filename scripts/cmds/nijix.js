bcbfc2986e11501a403501b3e2a5482b39281039
Tornanirechudi <forkkorlechudbo@gmail.com>
2024-06-30 14:04:25 +0600

    Update nijix.js

--- scripts/cmds/nijix.js
+++ scripts/cmds/nijix.js
@@ -1,6 +1,4 @@
 const axios = require("axios");
-const { getStreamFromURL } = global.utils;
-
 module.exports = {
     config: {
         name: "niji",
@@ -12,16 +10,30 @@ module.exports = {
         longDescription: "Text to Image",
         category: "ai",
         guide: {
-            en: "{pn} prompt --ar [ratio] or reply an image\n\n Example: {pn} 1girl, cute face, masterpiece, best quality --ar 16:9\n[ default 1:1 ]"
-        }
+    en: `{pn} <prompt> --ar [ratio], [preset], [style], or reply to an image\n\nAvailable Styles:\n1. Cinematic\n2. Photographic\n3. Anime\n4. Manga\n5. Digital Art\n6. Pixel Art\n7. Fantasy Art\n8. Neon Punk\n9. 3D Model\n\nAvailable Presets:\n1. Standard v3.0\n2. Standard v3.1\n3. Light v3.1\n4. Heavy v3.1`,
+      }
     },
 
     onStart: async function({ api, args, message, event }) {
         try {
             let prompt = "";
+            let style = "";
             let imageUrl = "";
+            let preset = "";
             let aspectRatio = ""; 
 
+            const styleIndex = args.indexOf("--style");
+            if (styleIndex !== -1 && args.length > styleIndex + 1) {
+                style = args[styleIndex + 1];
+                args.splice(styleIndex, 2); 
+            }
+
+            const presetIndex = args.indexOf("--preset");
+            if (presetIndex !== -1 && args.length > presetIndex + 1) {
+                preset = args[presetIndex + 1];
+                args.splice(presetIndex, 2); 
+            }
+            
             const aspectIndex = args.indexOf("--ar");
             if (aspectIndex !== -1 && args.length > aspectIndex + 1) {
                 aspectRatio = args[aspectIndex + 1];
@@ -40,7 +52,7 @@ module.exports = {
             }
 
             
-            let apiUrl = `https://project-niji.onrender.com/api/v1/generate?prompt=${encodeURIComponent(prompt)}.&aspectRatio=${aspectRatio}&apikey=rehat`;
+            let apiUrl = `https://rehatdesu.xyz/api/imagine/niji?prompt=${encodeURIComponent(prompt)}.&aspectRatio=${aspectRatio}&style=${style}&preset=${preset}&apikey=gaysex`;
             if (imageUrl) {
                 apiUrl += `&imageUrl=${imageUrl}`;
             }
@@ -50,7 +62,7 @@ module.exports = {
             const img = response.data.url;
 
             await message.reply({
-                attachment: await getStreamFromURL(img)
+                attachment: await global.utils.getStreamFromURL(img)
             });
 
         } catch (error) {
